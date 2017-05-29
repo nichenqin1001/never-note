e8e92785d29e5920cfb1b077904153de3a713705
nichenqin1001 <nichenqin@hotmail.com>
2017-05-29 20:34:58 +0800

    feature with animate.css & font-awesome

--- client/components/Feature/FeatureMain.js
+++ client/components/Feature/FeatureMain.js
@@ -16,7 +16,11 @@ class FeatureMain extends Component {
       <div className="feature__main">
         <h1 className="feature__main-header">Never Note</h1>
         <p className="feature__main-content">我们的目标是：不记笔记！</p>
-        <button onClick={this.onDemoLogin.bind(this)} className="button button__large button__focus">Live Demo</button>
+        <div className="flex flex__center">
+          <i className="fa fa-chevron-right fa-3x animated bounceInLeft"></i>
+          <button style={{ margin: '0 20px' }} onClick={this.onDemoLogin.bind(this)} className="button button__large button__focus">Live Demo</button>
+          <i className="fa fa-chevron-left fa-3x animated bounceInRight"></i>
+        </div>
         <div className="feature__main-description">
           <img width="256" height="236" src="/images/notebook.png" alt="" />
         </div>
