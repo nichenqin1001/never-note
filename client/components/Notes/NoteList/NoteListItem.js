40c0b67d226043315002f9796c18027d0d66d6c1
nichenqin1001 <nichenqin@hotmail.com>
2017-05-31 15:32:10 +0800

    active nav link

--- client/components/Notes/NoteList/NoteListItem.js
+++ client/components/Notes/NoteList/NoteListItem.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter, NavLink } from 'react-router-dom';
 import moment from 'moment';
 
 class NoteListItem extends Component {
   render() {
     const { title, content, updatedAt, _id } = this.props.note;
     return (
-      <div className="notes__list__note">
-        <Link className="notes__list__note-content" to={`${this.props.match.path}/${_id}`}>
-          <h4 className="notes__list__note-header">{title || '还没有标题'}</h4>
-          <div className="notes__list__note-update">{moment(updatedAt).format('YYYY-MM-DD')}</div>
-          <p className="notes__list__note-para">{content || '还没有内容'}</p>
-        </Link>
-      </div>
+      <NavLink className="notes__list__note" to={`${this.props.match.path}/${_id}`}>
+        <div className="notes__list__note-content">
+          <h4>{title}</h4>
+          <p>{moment(updatedAt).format('YYYY-MM-DD')}</p>
+          <p>{content}</p>
+        </div>
+      </NavLink>
     );
   }
 }
