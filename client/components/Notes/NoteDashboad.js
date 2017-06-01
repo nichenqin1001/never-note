301f1d01a83b9578192d85713107e1fb17ca64b1
nichenqin1001 <nichenqin@hotmail.com>
2017-06-01 20:31:32 +0800

    get redux objext from parent

--- client/components/Notes/NoteDashboad.js
+++ client/components/Notes/NoteDashboad.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import { createContainer } from 'meteor/react-meteor-data';
-import { Meteor } from 'meteor/meteor';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
-// collections
-import { Notes } from '../../../imports/collections/notes';
 // components
 import Sidebar from './NoteSidebar';
 import NoteList from './NoteList/NoteList';
@@ -30,25 +26,5 @@ class NoteDashboad extends Component {
 };
 
 NoteDashboad = connect(({ isFullScreen, searchText }) => ({ isFullScreen, searchText }), actions)(NoteDashboad);
-
-NoteList = createContainer(props => {
-  const notesHandle = Meteor.subscribe('notes');
-  const loading = !notesHandle.ready();
-  const note = Notes.findOne();
-  const noteExists = !loading && !!note;
-  const notes = Notes
-    .find(
-    {
-      $or: [
-        { title: { $regex: props.searchText } },
-        { content: { $regex: props.searchText } },
-      ]
-    },
-    { sort: { updatedAt: -1 } }
-    )
-    .fetch();
-  const notesCount = notes.length;
-  return { notes, loading, noteExists, notesCount };
-}, NoteList);
 
 export default NoteDashboad;
