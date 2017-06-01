66ca658cb20b23ad72d3a5f7fe498b262751b2f2
nichenqin1001 <nichenqin@hotmail.com>
2017-06-01 13:08:46 +0800

    update to meteor 1.5

--- client/components/Feature/FeatureMain.js
+++ client/components/Feature/FeatureMain.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import faker from 'faker';
-import { Accounts } from 'meteor/accounts-base';
 import { withRouter } from 'react-router-dom';
 import { Notes } from '../../../imports/collections/notes';
 
