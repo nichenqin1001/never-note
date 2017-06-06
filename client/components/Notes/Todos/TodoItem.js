2dc22c68e3cd1badc42325a6169f6a34112226c2
nichenqin1001 <nichenqin@hotmail.com>
2017-06-06 23:01:48 +0800

    to replace rawHTML string

--- client/components/Notes/Todos/TodoItem.js
+++ client/components/Notes/Todos/TodoItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const TodoItem = ({ todoItem }) => {
+  const checked = todoItem.substring(4, 7) === '[x]';
+  const rawHTML = todoItem.replace('<li>[ ]', `<li><input type="checkbox" disabled checked=${checked}  />`);
   return (
-    <div className="todo__list-item" dangerouslySetInnerHTML={{ __html: todoItem }} />
+    <div className="todo__list-item" dangerouslySetInnerHTML={{ __html: rawHTML }} />
   );
 };
 
