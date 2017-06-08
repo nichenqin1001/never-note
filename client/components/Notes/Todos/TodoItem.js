40d41f5554200daca8aee0c7f3639f3461d92461
nichenqin1001 <nichenqin@hotmail.com>
2017-06-08 11:02:33 +0800

    render todo list item

--- client/components/Notes/Todos/TodoItem.js
+++ client/components/Notes/Todos/TodoItem.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const TodoItem = ({ todoItem }) => {
-  const checked = todoItem.substring(4, 7) === '[x]';
-  const rawHTML = todoItem.replace('<li>[ ]', `<li><input type="checkbox" disabled checked=${checked}  />`);
+  const checkeBoxStr = todoItem.substring(4, 7);
+  let rawHTML = '';
+  if (checkeBoxStr === '[ ]') {
+    rawHTML = todoItem.replace('<li>[ ]', `<li><input type="checkbox" disabled />`);
+  } else {
+    rawHTML = todoItem.replace('<li>[x]', `<li><input type="checkbox" disabled checked />`);
+  }
   return (
     <div className="todo__list-item" dangerouslySetInnerHTML={{ __html: rawHTML }} />
   );
